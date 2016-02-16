e2b390757921a378167794719e059f0538d2390f
Artur Petrosyan <a.petrosyan@live.nl>
2016-02-16 18:36:56 +0100

    initial

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+
+  def index
+  end
+
+
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+    redirect_to @post
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+private
+def post_params
+  params.require(:post).permit(:title, :body)
+end
+
+
+end
