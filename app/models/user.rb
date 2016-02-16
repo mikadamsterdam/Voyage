7a22174fa14a140861e96518afac4416f74302ea
navase <margot.sauter@gmail.com>
2016-02-16 17:32:41 +0100

    Added associations between users and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
    has_many :posts
+  has_many :active_relationships, class_name: "Relationship",
+    foreign_key: "follower_id", dependent: :destroy
 
 end
